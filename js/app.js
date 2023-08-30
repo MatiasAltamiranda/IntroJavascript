667ee26a760aef0f1b74d98f975b37226149ce3c
Matias <matias_lma94@hotmail.com>
2023-08-29 21:29:55 -0300

    commit

---
+++ js/app.js
@@ -0,0 +1,184 @@
+// Variable : Es un espacio en memoria que utilizamos para almacernar datos
+
+
+// declarar : Es cuando creo una variable
+// Inicializar : Es cuando le asigno un valor a la variable
+
+/* let miVariable = "hola 2";
+
+console.log(miVariable);
+ */
+// no pueden llevar espacios
+
+/*
+
+let segundaVariable// camelcase
+let tercera_variable // snakecase
+let CuartaVariable // cobolcase
+
+*/
+
+
+// no pueden llevar -
+
+
+//let quinta-variable
+
+// No pueden comenzar con un numero
+
+ //let variable88
+
+
+//Tipos de variables
+
+//let
+
+/* let total = 99;
+
+total=150;
+
+console.log(total); */
+
+//const
+
+/* const precio = 88;
+
+console.log(precio);
+ */
+//var
+
+/* var suma = 10;
+
+suma=52;
+
+console.log(suma); */
+
+// Tipos de datos
+
+
+
+/*  Primitivos   */
+
+// Numericos o numbers
+
+let numero1 = 55;
+let numero2 = 2.5;
+
+let suma = numero1+numero2;
+
+/* console.log(suma) */
+
+
+// String o cadenas de texto
+
+let saludo = 'Hola'
+
+
+/* console.log(saludo) */
+
+// concatenar variables
+
+let nombre = "soy Matias"
+
+
+/* console.log(saludo + " " +nombre) */
+
+// Interpolacion
+
+/* console.log(`${saludo} ${nombre}`) */
+
+// Booleanos
+
+let isTrue = true; //1
+let isFalse = false; //0
+
+//null
+
+let datoNulo = null;
+
+/*  De objeto   */
+
+//array, arreglos y matriz
+
+let array = ["hola", false,45,datoNulo];
+let array2 = [[[]],[[]]];
+
+
+/* console.log(array); */
+
+// objeto
+
+let persona = {
+    nombre : "Elias",
+    apellido : "Fernandez",
+    edad : 35
+}
+
+/* console.log(persona)
+ */
+
+
+// tipos de operadores
+
+// Aritmeticos
+
+let datoA= 4;
+let datoB= 3;
+
+/* 
++
+-
+/
+%
+ */
+
+/* let suma2 = datoA + datoB;
+console.log(suma2)
+let resta = datoA-datoB;
+console.log(resta);
+let multiplicacion = datoA * datoB;
+console.log(multiplicacion)
+let division = datoA/datoB;
+console.log(Math.floor(division));
+let resto = datoA % datoB;
+console.log(resto);
+let ejercicio = (datoA+datoB)*3;
+console.log(ejercicio) */
+
+// unitarios
+
+let datoC = 5;
+let datoD = 10;
+let datoE = 5;
+
+/*
+++datoC;
+--datoD; 
+*/
+
+/* console.log(datoC);
+console.log(datoD); */
+
+//comparacion o relacional
+
+
+/* 
+
+<
+>
+<=
+>=
+==
+===
+
+*/
+
+console.log(datoC<datoC)
+console.log(datoC>datoD)
+console.log(datoC<=datoE)
+console.log(datoC>=datoD)
+console.log(datoC == datoE)
+console.log(datoC != datoE)
+console.log(datoC === datoE)
+console.log(datoC!==datoE)
+
